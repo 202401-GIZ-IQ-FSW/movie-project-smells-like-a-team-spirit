87862e8de825f4d1f7fb607e12a40a73dbf0384d
DRACOWRLD999 <hmoodhmood004@gmail.com>
2024-03-19 20:40:54 +0300

    added themes

---
+++ app/components/NavBar/NavBar.jsx
@@ -0,0 +1,31 @@
+'use client'
+
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+    navigationMenuTriggerStyle,
+  } from "@/components/ui/navigation-menu"
+import Link from "next/link";
+
+  import React, { createContext } from 'react';
+  export default function NavBar() {
+
+    return (
+        <div className="flex flex-row  items-center">
+            <NavigationMenu>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <Link href="/">Home</Link>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+        </div>
+    )
+  }
+  
