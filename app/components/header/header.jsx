975f0308d090290d375f88d3030d515591d4645b
aland rebwar <rebwar.aland11@gmail.com>
2024-03-25 04:19:04 +0300

    header update for the new navbar

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -10,21 +10,6 @@ export default function Header() {
           Late Night Cinema
         </span>
       </div>
-{/* 
-      <div className="flex items-center sm:flex-grow ">
-         <FaSearch className="sm:hidden cursor-pointer hover:text-amber-500" />
-         <input
-          type="text"
-          placeholder="Search Keywords"
-          className="w-full h-10 px-4 rounded-lg  focus:outline-none hidden sm:block focus:bg-white mx-auto"
-        /> 
-      </div> */}
-
-      {/* <div className="flex gap-5">
-        <div>
-          <DarkModeSwitch />
-        </div>
-      </div> */}
     </div>
   )
 }
