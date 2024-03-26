ecdeb0a2fe00587ef6835de11833b98af52426a3
DRACOWRLD999 <hmoodhmood004@gmail.com>
2024-03-26 20:15:07 +0300

    Refactor SearchResultItem component

--- app/(pages)/SearchResults/SearchResultItem.jsx
+++ app/(pages)/SearchResults/SearchResultItem.jsx
@@ -4,21 +4,20 @@ import React from "react";
 
 const SearchResultItem = ({ movie }) => {
   return (
-    <div className=" flex flex-row py-2 px-4 cursor-pointer bg-primary-foreground outline hover:bg-secondary">
-      <Link href={`/movie/${movie.id}`}>
-      <Image
-        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-        alt={movie.title}
-        width={50}
-        height={60}
-        
-      />
+    <Link href={`/movie/${movie.id}`}>
+      <div className=" flex flex-row py-2 px-4 cursor-pointer bg-primary-foreground outline hover:bg-secondary">
+        <Image
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+          width={50}
+          height={60}
+        />
         <div className="felx flex-col  items-center justify-center text-secondary-foreground  pl-1">
           <h1>{movie.title}</h1>
           <p>{movie.release_date}</p>
         </div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 };
 
