8c22ca84ca542ec07c0fb530ac93e8799c41d69f
DRACOWRLD999 <hmoodhmood004@gmail.com>
2024-03-26 17:29:27 +0300

    Remove unused imports from movie-genre and tv-genre pages

--- app/(pages)/tv-genre/page.jsx
+++ app/(pages)/tv-genre/page.jsx
@@ -1,7 +1,5 @@
 "use client";
 import React from "react";
-import MovieCards from "../components/MovieCard/MovieCards";
-import { useParams } from "next/navigation";
 
 export default function Page({ params }) {
   return (
